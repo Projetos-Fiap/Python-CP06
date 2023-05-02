85d0911158687459ff59c0d66f13de5ceba590d9
andrelambert <rm99148@fiap.com.br>
2023-05-02 11:43:53 -0300

    primeiro commit

---
+++ CP02.py
@@ -0,0 +1,5 @@
+# CP02 - Python - Turma 1ESPW
+# Membros do grupo: André Lambert (RM99148), Alessandra Vaiano (RM551497), 
+# Bryan William (RM551305), Lucas Feijó (RM99727) e Vitor Maia (RM99658).
+#
+#
